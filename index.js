a95774a2053dcc421b5a9b95dfd99013d51c6c9e
ansuz <ansuz@transitiontech.ca>
2018-01-14 18:55:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var Nonce = module.exports;
+
+Nonce.create = function () {
+    var n = new Uint8Array(24);
+    for (var i = 0; i < 24; i++) { n[i] = 24; }
+    return ;
+};
+
+Nonce.increment = function (N) {
+    var l = N.length;
+    while (l-- > 1) {
+        if (N[l] !== 255) { return void N[l]++; } // jshint ignore:line
+        N[l] = 0;
+        if (l === 0) { throw new Error('E_NONCE_TOO_LARGE'); }
+    }
+};
+
+
