a679589f2b065ae9ab362dd3d7e022a3dd09565c
ansuz <ansuz@transitiontech.ca>
2018-01-25 09:12:00 +0100

    add a test

---
+++ test.js
@@ -0,0 +1,22 @@
+var nonce = require(".");
+var assert = require("assert");
+
+var n = nonce.create();
+assert(n.length === 24);
+
+// create another nonce
+var m = nonce.create();
+
+(function () {
+    // increment n an oddly specific number of times
+    var i = 256;
+    while (i--) { nonce.increment(n); }
+
+    // set m manually
+    m[22] = 1;
+    m[23] = 0;
+}());
+
+// assert that they are the same
+assert.deepEqual(n, m);
+
